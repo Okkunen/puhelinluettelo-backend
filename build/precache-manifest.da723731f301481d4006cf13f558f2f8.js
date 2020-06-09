2104e6e631b949fe1733e8b0195fe55d60556dee
Okkunen <tsemi@hotmail.com>
2020-06-09 16:58:16 +0300

    front end mukaan

---
+++ build/precache-manifest.da723731f301481d4006cf13f558f2f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49095ffe26d8c503f618f07429d1ef5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b24f8e090db32eabc4c7",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "cff00fb6e6ff9b32d401",
+    "url": "/static/js/2.9561ceca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9561ceca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b24f8e090db32eabc4c7",
+    "url": "/static/js/main.74b9f3d2.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
